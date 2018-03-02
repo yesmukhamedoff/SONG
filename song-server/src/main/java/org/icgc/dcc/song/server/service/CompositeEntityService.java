4a69e945073dcaa8aa369adaf4f55baa6230a754
rtisma <rtisma@gmail.com>
2018-03-02 01:05:59 -0500

    updated

--- song-server/src/main/java/org/icgc/dcc/song/server/service/CompositeEntityService.java
+++ song-server/src/main/java/org/icgc/dcc/song/server/service/CompositeEntityService.java
@@ -2,6 +2,7 @@ package org.icgc.dcc.song.server.service;
 
 import lombok.AllArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
 import org.icgc.dcc.song.server.model.entity.composites.CompositeEntity;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import static java.util.Objects.isNull;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class CompositeEntityService {
@@ -25,10 +27,13 @@ public class CompositeEntityService {
 
   public String save(String studyId, CompositeEntity s) {
     String id = sampleService.findByBusinessKey(studyId, s.getSampleSubmitterId());
+    log.info("ID = {},  StudyId={}, SampleSubmitterId= {}", id, studyId, s.getSampleSubmitterId());
     if (isNull(id)) {
       s.setSpecimenId(getSampleParent(studyId, s));
       id = sampleService.create(studyId, s);
+      log.info("CREATED SAMPLE_ID= {}", id);
     } else {
+      log.info("UPDATING SAMPLE: {}", s);
       sampleService.update(s);
     }
 
